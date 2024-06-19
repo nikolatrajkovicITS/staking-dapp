7b632a53b192bf36a42df1ef58957444661a0a41
nikola <nikolatrajkovic.contact@gmail.com>
2024-06-19 15:49:18 +0200

    chore: install libs remove extra files

---
+++ src/app/_app.ts
@@ -0,0 +1,14 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import theme from "../theme";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
